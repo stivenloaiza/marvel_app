ec00a18dbc3cdb2e18269db9813cbf6f8d81fec7
Stiven Loaiza <loaizadeveloper@gmail.com>
2021-05-04 01:08:06 -0500

    se crea paleta de colores, service conecction marvel y archivo de configuración general

--- lib/configuration.dart
+++ lib/configuration.dart
@@ -1,6 +1,20 @@
+import 'package:flutter/material.dart';
+
 //variables globales
 final String pubKMarvelAPI="5bfdaaccb35e714e956317cab76cd42d";
 final String priKMarvelAPI="db768e1afd5c1b9f5f3e48ad950e07cba7c5edeb";
 final String hashMarvelAPI="ac5735a846871348f3b3390cec0fada5";
 final String tsMarvelAPI="1";
 final String urlApiMarvel="https://gateway.marvel.com:443/v1";
+
+final String nombreApp="MarvelAPP";
+
+//Colors
+final Color primaryMoradoOscuro = Color(0XFF5432D3);
+final Color primaryMoradoclaro = Color(0XFFB224EF);
+final Color primaryRojo = Color(0XFFF2264B);
+final Color primaryNegro = Color(0XFF313140);
+final Color secundaryAzul = Color(0XFF005BEA);
+final Color secundaryAzulClaro = Color(0XFF00C6FB);
+final Color neutralBlanco = Color(0XFFF5F5F5);
+final Color neutralAzulclaro = Color(0XFFC5E3F6);
